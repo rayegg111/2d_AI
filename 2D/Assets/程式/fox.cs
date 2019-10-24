5dad17ad804bd21256492236a533fe6ab63e4dab
rayegg111 <55218513+rayegg111@users.noreply.github.com>
2019-10-24 12:03:43 +0800

    123

--- 2D/Assets/程式/fox.cs
+++ 2D/Assets/程式/fox.cs
@@ -3,8 +3,36 @@ using UnityEngine.AI;
 
 public class fox : MonoBehaviour
 {
+    // 成員：欄位、屬性、方法、事件
+    // 修飾詞 類型 名稱 指定 值；
+    public int speed = 15;                  // 整數
+    public float jump = 2.5f;               // 浮點數
+    public string foxName = "狐狸";         // 字串
+    public bool pass = false;               // 布林值 - true/false
+
+    private Rigidbody2D r2d;
+    private Transform tra;
+
+    // 事件：在特定時間點會以指定頻率執行的方法
+    // 開始事件：遊戲開始時執行一次
     private void Start()
     {
-        //Debug.Log("Hello This Word");
+        // 泛型 <T>
+        r2d = GetComponent<Rigidbody2D>();
+        tra = GetComponent<Transform>();
+    }
+
+    //更新事件：每秒約執行60次
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D)) tra.eulerAngles = new Vector3(0, 0, 0);
+        if (Input.GetKeyDown(KeyCode.A)) tra.eulerAngles = new Vector3(0, 180, 0);
+    }
+
+    //固定更新事件：每禎0.002秒
+    private void FixedUpdate()
+    {
+        //Debug.Log(Input.GetAxis("Horizontal"));
+        r2d.AddForce(new Vector2(speed * (Input.GetAxis("Horizontal")), 0));
     }
 }
