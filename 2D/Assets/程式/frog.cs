7205d25a4c62bcb1c292c0c5ed77158a19bb8fb8
rayegg111 <55218513+rayegg111@users.noreply.github.com>
2019-11-14 12:06:12 +0800

    11/14

--- 2D/Assets/程式/frog.cs
+++ 2D/Assets/程式/frog.cs
@@ -1,9 +1,21 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class frog : MonoBehaviour
 {
+    #region 欄位/屬性
+    // 定義列舉
+    // 修飾詞 列舉 列舉名稱 { 列舉內容, .... }
+    public enum state
+    {
+        // 一般、尚未完成、完成
+        star, notComplete, complete
+    }
+    // 使用列舉
+    // 修飾詞 類型 名稱
+    public state _state;
+
+
     [Header ("任務對話")]
     public string star = "嘿，幫我找5顆鑽石嗎?";
     public string not_complete = "你還沒找到5顆鑽石，等你找到再來找我吧";
@@ -14,16 +26,47 @@ public class frog : MonoBehaviour
     public bool mission_complete = false;
     public int count_player = 0;
     public int finish = 5;
+    [Header("UI")]
+    public GameObject objcan;
+    public Text textSay;
+    #endregion
 
-	// Use this for initialization
-	void Start ()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(star);
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        if (collision.name == "狐狸")
+        {
+            Say();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
     {
-		
-	}
+        Sayout();
+    }
+
+    /// <summary>
+    /// 對話
+    /// </summary>
+    void Say()
+    {
+        objcan.SetActive(true);
+        textSay.text = star;
+        switch (_state)
+        {
+            case star:
+                print("玩家輸入的是黑色");
+                break;
+            case notcomplete:
+                print("玩家輸入的是紅色");
+                break;
+        }
+    }
+
+    /// <summary>
+    /// 離開對話
+    /// </summary>
+    void Sayout()
+    {
+        objcan.SetActive(false);
+    }
 }
